b0274201f8b16ab011f1d2277bb0a3e364effe2a
Shilpisingh220 <shilpibssingh27@gmail.com>
2024-11-13 14:38:17 +0000

    implement random

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
 import "./index.css";
+
+import { formEl, inputEl, yearEl } from "./domSelection.js";
+
+const tasks =[];
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+  if (!inputEl.value){
+  return;
+}
+  tasks.push({title : inputEl.value, iscompleted : false, id: crypto.randomUUID()});
+  console.log(tasks);
+
+  inputEl.value = "";
+});
+
+
+// IIFE
+(function () {
+  const year = new Date().getFullYear();
+
+  // MARK: DOM update
+  yearEl.textContent = `${year}`;
+})();
