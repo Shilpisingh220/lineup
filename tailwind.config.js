d4ddcb11d28e6db2504b35dea318a6b9e847f93b
Shilpisingh220 <shilpibssingh27@gmail.com>
2024-11-13 12:25:35 +0000

    theme complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
-    "./src/**/*.{js}",
-  ],
+  content: ["./index.html", "./src/**/*.{js}"],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui'),],
-}
-
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "synthwave"],
+  },
+};
